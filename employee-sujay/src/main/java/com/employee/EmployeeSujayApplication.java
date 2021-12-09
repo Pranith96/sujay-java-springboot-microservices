2f63501f396704b831de66f950f1ea1807f6c211
Your Name <you@example.com>
2021-12-09 10:13:18 +0530

    employee project

---
+++ employee-sujay/src/main/java/com/employee/EmployeeSujayApplication.java
@@ -0,0 +1,13 @@
+package com.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSujayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSujayApplication.class, args);
+	}
+
+}
